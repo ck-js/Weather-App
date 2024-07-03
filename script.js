7583468af26660b35eafc0d325ebe2516d6961ff
ck-js <ckentertainmentinfo@gmail.com>
2024-07-03 11:49:31 +0700

    de couble funtionality and operations for section 2 by calling create card item function and passing the response object which we manipulate the time property as we only want to display the hour hour time format with 0 prefixed if the hour is less than 10. All time and temp c values are appended to DOM in their own card item.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const overview = document.querySelector('#overview');
 const overviewImage = document.querySelector('#overview-image');
 const highLow = document.querySelector('#high-low');
 
+const section2Container = document.querySelector('#section-2-container');
+
+
 function fetchNewLocation(location) {
 // alert(location)
 
@@ -15,12 +18,18 @@ fetch(`https://api.weatherapi.com/v1/forecast.json?key=06b893ef0f8344fc8d0016512
   })
   .then(function(response) {
     console.log(response);
+    // section 1
     name.innerHTML = response.location.name;
     tempC.innerHTML = response.current.temp_c + '°C';
     overview.innerHTML = response.current.condition.text;
     overviewImage.src = response.current.condition.icon;
     overviewImage.alt = response.current.condition.text;
     highLow.innerHTML = `H : ${response.forecast.forecastday[0].day.maxtemp_c}°  L : ${response.forecast.forecastday[0].day.mintemp_c}°`;
+
+// section 2
+createCardItem(response)
+
+
 
 
   })
@@ -29,9 +38,35 @@ fetch(`https://api.weatherapi.com/v1/forecast.json?key=06b893ef0f8344fc8d0016512
     name.innerHTML = 'Location not found';
     tempC.innerHTML = '°';
   });
-
-
 }
+
+
+function createCardItem(object) {
+const hourly = object.forecast.forecastday[0].hour;
+    for (let i = 0; i < hourly.length; i++) {
+        const hourlyTime = hourly[i].time;
+        const date = new Date(hourlyTime)
+        const formattedHourlyTime = (date.getHours() < 10 ? '0' : + '') + date.getHours();
+        
+        const hourlyTemp = hourly[i].temp_c;
+    
+        const hourlyDiv = document.createElement('div');
+    const p = document.createElement('h6');
+    p.textContent = formattedHourlyTime;
+    
+    const p2 = document.createElement('p');
+    p2.textContent = hourlyTemp + '°'
+    
+    hourlyDiv.appendChild(p)
+    hourlyDiv.appendChild(p2)
+    
+    section2Container.appendChild(hourlyDiv);
+
+
+    }
+}
+
+
 
 const searchInput = document.getElementById('search-input');
 const button = document.querySelector('button')
