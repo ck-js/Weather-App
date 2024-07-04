a98323cadce7edb27993a4ff2a8d6cc9ace9b53b
ck-js <ckentertainmentinfo@gmail.com>
2024-07-04 15:34:51 +0700

    implement the daily forecast section with daily container containing name of the day in short form and condition wrapper which nests condition icon and chance of rain for each day. Use the toLocalDateString method to transform date object to sstring which was originally a string date from the API and converted into date object to be able to get the name of the day.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const overviewImage = document.querySelector('#overview-image');
 const highLow = document.querySelector('#high-low');
 
 const section2Container = document.querySelector('#section-2-container');
+const section3Container = document.querySelector('#section-3-container');
 
 
 function fetchNewLocation(location) {
@@ -28,8 +29,7 @@ fetch(`https://api.weatherapi.com/v1/forecast.json?key=06b893ef0f8344fc8d0016512
 
 // section 2
 createSection2Cards(response)
-
-
+createSection3Cards(response)
 
 
   })
@@ -82,9 +82,40 @@ hourlyDiv.appendChild(p3)
 
     }
 }
+function createSection3Cards(object) {
+const dailyForecast = object.forecast.forecastday;
+for (let i = 0; i < dailyForecast.length; i++) {
+    const dailyForecastObject = dailyForecast[i];
 
+ const dailyForecastDate = new Date(dailyForecast[i].date);   
+        const formattedDay = dailyForecastDate.toLocaleDateString('en-US', { weekday: 'short' });
 
+ const cardElement = document.createElement('div');
+    cardElement.classList.add('daily-forecast-cards');
 
+const h6 = document.createElement('h6');
+h6.textContent = formattedDay;
+cardElement.appendChild(h6)
+
+const dailyConditionWrapper = document.createElement('div');
+dailyConditionWrapper.classList.add('daily-condition-wrappers');
+
+const dailyConditionImage = document.createElement('img');
+dailyConditionImage.src = dailyForecastObject.day.condition.icon;
+dailyConditionImage.alt = dailyForecastObject.day.condition.text;
+
+const dailyChanceOfRain = document.createElement('p');
+dailyChanceOfRain.textContent = dailyForecastObject.day.daily_chance_of_rain + '%';
+
+dailyConditionWrapper.appendChild(dailyConditionImage);
+dailyConditionWrapper.appendChild(dailyChanceOfRain);
+
+cardElement.appendChild(dailyConditionWrapper);
+
+section3Container.appendChild(cardElement);
+}
+
+}
 const searchInput = document.getElementById('search-input');
 const button = document.querySelector('button')
 button.addEventListener('click', event => {
